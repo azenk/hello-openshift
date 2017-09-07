4ac4def1ee0f89f1995e87a0d4a136d4debca789
Andrew Zenk <azenk@umn.edu>
2017-09-06 20:51:12 -0500

    Print pod IP and hostname

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
+<?php header("Refresh:2") ?>
 <html>
 <head><title>Hello Openshift!</title></head>
 <body>
 <h1>Hello Openshift!</h1>
+<p>This pod is running on <?php print $_SERVER['SERVER_ADDR'] ?> whose name is <?php print getenv('HOSTNAME') ?>.</p>
 </body>
 </html>
