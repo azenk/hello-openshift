f048f2fbab78fb1ecb5ab7c75a37ebc7c1b50855
Andrew Zenk <azenk@umn.edu>
2017-09-06 21:14:05 -0500

    Generate a random color for the pod and display it
    It's hard to spot the changing IP/pod name. Generating a random color
    using a md5 hash of the IP address should provide a more visible
    indicator.

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
 <?php header("Refresh:2") ?>
+<?php
+  $ip = $_SERVER['SERVER_ADDR'];
+  $pod_name = getenv('HOSTNAME');
+  $hash = hash('md5', $ip, true);
+  $color_string = substr(bin2hex($hash), -6);
+?>
 <html>
 <head><title>Hello Openshift!</title></head>
 <body>
 <h1>Hello Openshift!</h1>
-<p>This pod is running on <?php print $_SERVER['SERVER_ADDR'] ?> whose name is <?php print getenv('HOSTNAME') ?>.</p>
+<p>This pod is running on <?php print $ip ?> whose name is <?php print $pod_name ?>.</p>
+<p>This pod's color is <div style="background-color: <?php print $color_string; ?>; width: 5em; height: 5em;"</div></p>
 </body>
 </html>
